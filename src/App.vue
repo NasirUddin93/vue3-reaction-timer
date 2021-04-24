637e6a92f55b87ed882bc1bad40a4db23517d240
NasirUddin93 <nasir93cse@gmail.com>
2021-04-24 14:40:05 +0600

    vue 3 Reaction Timer is successfully done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Nasir Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Results v-if="showResults" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from './components/Block'
+import Results from './components/Results.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+      Block,
+    Results,
+  },
+  data(){
+    return {
+      isPlaying:false,
+      delay:null,
+      score:null,
+      showResults:false
+    }
+  },
+  methods:{
+    start(){
+      this.delay = 200 + Math.random() * 5;
+      this.isPlaying = true;
+      this.showResults = false
+
+    },
+    endGame(reactionTime){
+        this.score = reactionTime
+        this.isPlaying = false,
+        this.showResults = true
+    },
+  },
 }
 </script>
 
@@ -20,7 +44,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
